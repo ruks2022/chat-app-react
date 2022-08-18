697a34e5903d08fe79af705bc35a5ac8509ba7da
ruks2022 <shaikhruksar2022@gmail.com>
2022-08-18 09:52:51 +0530

    version-1

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div>
+      <p>Hello World!</p>
+    </div>      
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+//New imports
+const http = require('http').Server(app);
+const cors = require('cors');
+
+app.use(cors());
+
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'Hello world',
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+//New imports
+
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
+//Add this before the app.get() block
+socketIO.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`);
+    socket.on('disconnect', () => {
+      console.log('🔥: A user disconnected');
+    });
+});
